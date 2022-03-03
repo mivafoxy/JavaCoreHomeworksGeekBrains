86045e4a811d21176b6bff3abb89570bace005b8
mivafox <creatox1996@gmail.com>
2022-03-03 21:43:40 +0300

    Выполненная 5 домашка

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
@@ -30,6 +32,15 @@ public class Main {
         try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
             byte[] csvData = newData().toString().getBytes(StandardCharsets.UTF_8);
             fileOutputStream.write(csvData);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream(FILE_NAME)) {
+            byte[] csvData = new byte[fileInputStream.available()];
+            fileInputStream.read(csvData);
+            String table = new String(csvData);
+            System.out.println(table);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
