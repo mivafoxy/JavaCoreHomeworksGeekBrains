7deac9375ebf842baef49b50c335805cf2624da6
mivafox <creatox1996@gmail.com>
2022-03-10 13:41:21 +0300

    Выполненная 6 домашка

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,32 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-/*
-1. Реализовать сохранение данных в csv файл;
-2. Реализовать загрузку данных из csv файла. Файл читается целиком.
-Структура csv файла:
-| Строка заголовок с набором столбцов |
-| Набор строк с целочисленными значениями |
-| * Разделитель между столбцами - символ точка с запятой (;) |
-
-Пример:
-Value 1;Value 2;Value 3
-100;200;123
-300,400,500
-Для хранения данных использовать класс вида:
-public class AppData {
-  private String[] header;
-  private int[][] data;
-
- // ...
-}
-Если выполняется save(AppData data), то старые данные в файле полностью перезаписываются.
- */
 public class Main {
-    public static final String FILE_NAME = "table.csv";
+    private final static String WeatherURL =  "http://dataservice.accuweather.com/forecasts/v1/daily/5day/294021"; // URL для запроса погоды на 5 дней в СПБ.
+    private final static String API_KEY = "?apikey=ТУТ_НУЖНО_ВСТАВИТЬ_КЛЮЧ_API"; // API ключ для запроса. См. гайд, как его получить и вставить.
 
     public static void main(String[] args) {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
-            byte[] csvData = newData().toString().getBytes(StandardCharsets.UTF_8);
-            fileOutputStream.write(csvData);
+        try {
+            URL weatherUrl = new URL(WeatherURL+API_KEY); // Сформировали URL для запроса к серверу.
+            HttpURLConnection urlConnection = (HttpURLConnection) weatherUrl.openConnection(); // К серверу постучались.
+            if (urlConnection.getResponseCode() == 200) { // getResponseCode отправляет запрос к серверу по указанному нами URL, который по факту является GET запросом.
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) { // Как было на предыдущих занятиях, чтобы считать данные из
+                    StringBuilder responseContent = new StringBuilder();                                                  // сети, необходимо открыть Stream для их чтения.
+                    String line = "";                                                                                     // Тут мы используем для работы с сетью BufferedReader.
+                    while ((line = reader.readLine()) != null) { // Считываем данные от сервера до конца (тут нет EOF, как в случае с фалами. Если данных нет от сервера, то метод readLine()
+                        responseContent.append(line);            // вернет null.
+                    }
+                    System.out.println(responseContent);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-
-        try (FileInputStream fileInputStream = new FileInputStream(FILE_NAME)) {
-            byte[] csvData = new byte[fileInputStream.available()];
-            fileInputStream.read(csvData);
-            String table = new String(csvData);
-            System.out.println(table);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public static AppData newData() {
-        AppData appData = new AppData();
-        String[] headers = new String[] { "title 1", "title 2", "title 3" };
-        appData.init(headers);
-        return appData;
     }
 }
